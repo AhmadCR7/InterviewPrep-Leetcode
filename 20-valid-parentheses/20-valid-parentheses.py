df82ec31e6dc3e88ed6e94d90c432b85de1dad99
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-04-19 11:33:30 -0700

    Time: 38 ms (67.00%), Space: 14 MB (26.67%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,24 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # we use hashmap to match if close parentheses matches with open one 
-        # we create stack on top to match them
-        # we repeat this process until our stack is empty and we match all the {} 
-        # the time complexity is O(n) since we got to each input charactor once 
-        # memory use will be O(n) sice we use the size of stack 
         stack = []
-        closeToOpen = {")" : "(", "]" : "[", "}" : "{" }
-
-        # go throgh every input charactor 
-        for char in s: 
-            if char in closeToOpen: # check if this charactor is closing prantices (bc all the charctor are key) 
-                if stack and stack[-1] == closeToOpen[char]: # find the open one on top of the stack and match them 
+        close_to_open = {")" : "(", "]" : "[", "}" : "{"}
+        
+        for char in s:
+            if char in close_to_open:
+                if stack and stack[-1] == close_to_open[char]:
                     stack.pop()
-                else: 
+                else:
                     return False 
             else:
-                stack.append(char) ## add as many as pranteses you want
-        # return true only if the stack is empty 
-        return True if not stack else False
+                stack.append(char)
+        return True if not stack else False 
         
         
         
