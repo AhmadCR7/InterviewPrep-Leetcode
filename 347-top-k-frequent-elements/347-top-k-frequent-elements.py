66e11d7a309b18b73b58a07399db2d656b6bd8a5
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-05-09 14:17:47 -0700

    Time: 120 ms (66.56%), Space: 18.6 MB (90.89%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if k == len(nums):
+            return nums
+        # # 1. build hash map : character and how often it appears
+        count = Counter(nums)
+        
+        return heapq.nlargest(k, count.keys(), key = count.get)
+        
+        
+        
+        
+# we build hasmap "element --> frequency"
+# [1,1,1,2,2,2,3,3,4,5,5,5,5]
+# 1 -> 3
+# 2 -> 3
+# 3 -> 2
+# 4 -> 1
+# 5 -> 4
+# build heap of k most frequent elements 
+# 1 2 5
+# build an output array 
+# [2,5,1]
