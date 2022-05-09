3608475e04bb8a142337a1d44e95e4bd7825b2e6
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-05-09 14:50:30 -0700

    Time: 2289 ms (25.63%), Space: 27.6 MB (50.98%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,6 +1,8 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        #create a set
         numSet = set(nums)
+        # longest consacative seq initally it's 0
         longest = 0
         
         for n in nums:
@@ -11,3 +13,15 @@ class Solution:
                     length += 1
                 longest = max(length, longest)
         return longest
+    
+    
+    
+    
+# we create a set using our array 
+#set nums = [100,4,200,1,3,2]
+# we check if the 100 is the first element of the sequence 100 has no left number meaning there is no 99
+# 100 ->  lenght 1
+# 200 ->  lenght 1
+# 1 -> 2 -> 3 -> 4  lenght 4
+# time O(n) sicne we visit each elements once 
+# Space: O(n) sicne we created a set() to see the size of an array
