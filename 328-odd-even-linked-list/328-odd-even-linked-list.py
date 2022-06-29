ef73bdad56aa8b10710975cfa087b68e0e4c5125
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-06-29 11:14:22 -0700

    Time: 67 ms (47.47%), Space: 16.6 MB (29.09%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.py
+++ 328-odd-even-linked-list/328-odd-even-linked-list.py
@@ -6,31 +6,26 @@
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next or not head.next.next:
-            return head
-        oddptr = curr = head
-        evenptr = overhead = head.next
+            return head 
+        oddptr = curr = head 
+        evenptr = overhead = curr.next 
         i = 1
         
         while curr:
             if i > 2 and i % 2 != 0:
                 oddptr.next = curr
-                oddptr = oddptr.next 
+                oddptr = oddptr.next
             elif i > 2 and i % 2 == 0:
                 evenptr.next = curr
-                evenptr = evenptr.next 
+                evenptr = evenptr.next
             curr = curr.next 
             i += 1
         evenptr.next = None 
-        oddptr.next = overhead
+        oddptr.next = overhead 
+    
         return head 
-'''
-1. first we check if there is head 
-2. check if there is one node 
-3. check if there is odd node
-4. we set up and index numbder to see if we are in even or odd number 
-5. we don't care about first and second since we already point the pointers at them 
-6. 
-'''
+                
+                
                 
             
         
