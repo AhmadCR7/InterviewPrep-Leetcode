93e041e54e26bc6aa9fed2e747dd7d6e00eb6478
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-07-06 20:03:04 -0700

    Time: 53 ms (80.07%), Space: 16.3 MB (37.61%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.py
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.py
@@ -13,3 +13,14 @@ class Solution:
             if node.right: q.append(node.right)
             if node.left: q.append(node.left)
         return node.val
+'''
+we can use level order traversal with que
+we do it from right to left and the last node that is left will be the most left node in the tree
+we use double ended que or deque 
+we add from bot side of the que 
+we pop from most left side 
+we put its children to the que 
+an pop the left again 
+we add the right node first bc we want to pop the right first 
+
+'''
