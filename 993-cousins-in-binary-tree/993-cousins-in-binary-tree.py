503dde62299cfec4bd72dd67791d19c09fc0160c
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-07-06 19:09:53 -0700

    Time: 60 ms (24.47%), Space: 14 MB (10.88%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
@@ -11,16 +11,15 @@ class Solution:
         def dfs(node, parent, depth, x, y):
             if not node:
                 return 
-            dfs(node.left, node, depth + 1, x, y)
-            dfs(node.right, node, depth + 1, x, y)
+            dfs(node.left, node, depth+1, x,y)
+            dfs(node.right, node, depth+1, x,y)
             
             if node.val in (x, y):
                 tracker[node.val] = (parent, depth)
         
-        dfs(root, None, 0, x, y)
-        cousins = tracker.get(x) and tracker.get(y) and tracker[x][1] == tracker[y][1] and tracker[x][0] != tracker[y][0]
-        return cousins 
-            
+        dfs(root, None, 0, x,y)
+        cousins = tracker.get(x) and tracker.get(y) and tracker[x][1] == tracker [y][1] and tracker[x][0] != tracker[y][0]
+        return cousins
 '''
 1. we have to go through the tree and see if x and y are exist
 2. if so, track its depth for x and y and track its parent 
