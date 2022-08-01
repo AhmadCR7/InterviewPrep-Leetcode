5b522c4a6f09dcd0aa56a33e363bfae8f2e0b6f8
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-08-01 12:04:30 -0700

    Time: 83 ms (16.44%), Space: 14.2 MB (91.15%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -5,20 +5,17 @@ class Solution:
         while(l<=r):
             mid = (l+r)//2
             if target == nums[mid]:
-                return mid 
+                return mid
             
-            #check left sorted portion 
             if nums[l] <= nums[mid]:
                 if target > nums[mid] or target < nums[l]:
-                    l = mid+1
+                    l = mid + 1
                 else:
-                    r = mid-1
-            #check right sorted portion
+                    r = mid - 1
             else:
                 if target < nums[mid] or target > nums[r]:
-                    r = mid-1
+                    r = mid - 1
                 else:
-                    l = mid+1
+                    l = mid + 1
         return -1
-                    
         
