a2fcc2d47bb95b562ac33ad2f0157d79e6337afe
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-08-24 17:49:26 -0700

    Time: 93 ms (49.42%), Space: 14.2 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,14 +1,6 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        
-        x=Counter(ransomNote)
-        y=Counter(magazine)
-        for i,v in x.items():
-            if(x[i]<=y[i]):
-                continue
-            else:
-                return False
-        return True
+        return not collections.Counter(ransomNote) - collections.Counter(magazine)
         
 '''
 x=Counter(ransomNote)
