773c870175752f563b81716749fb285aa7e4c0af
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-08-29 14:16:09 -0700

    Time: 48 ms (69.04%), Space: 14.2 MB (39.51%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -3,26 +3,38 @@ class Solution:
         stack = []
         res = []
         
-        def backtrack(open_N, closed_N):
-            if open_N < n:
-                stack.append("(")
-                backtrack(open_N + 1, closed_N)
-                stack.pop()
-            if closed_N < open_N:
-                stack.append(")")
-                backtrack(open_N, closed_N + 1)
-                stack.pop()
-            if open_N == closed_N == n:
+        def backtrack(openN, closedN): 
+            if openN == closedN == n:
                 res.append("".join(stack))
-                return
-        backtrack(0,0)
-        return res
+                return 
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        backtrack(0, 0)
+        return res 
+
+    
     
         
         
         
-        
-        
+'''
+Solution using stack: 
+
+1. N = 3
+We need 3 open and 3 close parentheses
+Rules: - 3 open, 3 close and close < open 
+#only add open parentheses if open < n
+#only add a closing parentheses if closed < open 
+#valid IIF open == closed == n
+
+
+'''
         
         
 
