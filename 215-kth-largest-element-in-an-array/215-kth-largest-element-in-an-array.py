c379ed36f591aa5848f00e496dc57fd3772659eb
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-08-29 13:34:33 -0700

    Time: 2221 ms (14.50%), Space: 37.2 MB (6.83%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,33 +1,20 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def partition(left, right, pInx):
-            pivot = nums[pInx]
-            nums[pInx], nums[right] = nums[right], nums[pInx]
-            
-            storeIndex = left
-            
-            for i in range(left, right):
-                if nums[i] < pivot:
-                    nums[storeIndex], nums[i] = nums[i], nums[storeIndex]
-                    storeIndex += 1
-            
-            nums[right], nums[storeIndex] = nums[storeIndex], nums[right]
-            
-            return storeIndex
+        k = len(nums) - k
         
-        def select(left, right, k_smallest):
-            if left == right:
-                return nums[left]
-            pInx = random.randint(left, right)
-            pInx = partition(left, right, pInx)
-            if k_smallest == pInx:
-                return nums[k_smallest]
-            elif k_smallest < pInx:
-                return select(left, pInx - 1, k_smallest)
-            else:
-                return select(pInx + 1, right, k_smallest)
-        return select(0, len(nums)-1, len(nums) - k)
-    
+        def quickselect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+            
+            if p > k: return quickselect(l, p - 1)
+            elif p <k: return quickselect(p+1, r)
+            else:      return nums[p]
+        return quickselect(0, len(nums)-1)
+                
     
     
     
@@ -38,5 +25,21 @@ return nums[-k]
 
 time: O(nNlogk)
 Space: O(K) to store heap elements 
+
+Solution 2: 
+QuickSelect Algo
+
+[3,2,1,5,6,4]
+
+pivet = [4]
+[3,,,,,,]
+sift the p
+[3,2,1,5,6,4] the left side of the array is less than the pevit 
+swap the p with the pevit 
+[3,2,1,4,6,5]
+
+now lenght - k run quick sort on the right side 
+
+[3,2,1,4,5,6]
 
 '''
