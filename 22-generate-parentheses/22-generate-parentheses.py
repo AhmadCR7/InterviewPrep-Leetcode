db639856d94edf756877c8768b9dd2796967ed48
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-08-30 07:56:56 -0700

    Time: 61 ms (37.58%), Space: 14.2 MB (39.51%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -34,6 +34,22 @@ Rules: - 3 open, 3 close and close < open
 #valid IIF open == closed == n
 
 
+def backtrack(openN, closedN): 
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return 
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        backtrack(0, 0)
+        return res 
+
+
 '''
         
         
