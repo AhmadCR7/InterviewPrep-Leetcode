e0d99fa4beb4057f2b3e3037cda21b51b6ee4bb4
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-08-30 07:41:22 -0700

    Time: 39 ms (77.78%), Space: 14 MB (26.47%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,17 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        closeToOpen = {")" : "(", "]" : "[", "}" : "{" }
+        openToClose = {")" : "(", "]" : "[", "}" : "{" }
         
         for char in s:
-            if char in closeToOpen:
-                if stack and stack[-1] == closeToOpen[char]:
+            if char in openToClose:
+                if stack and stack[-1] in openToClose[char]:
                     stack.pop()
                 else:
                     return False 
             else:
                 stack.append(char)
         return True if not stack else False 
+            
         
         
 '''
