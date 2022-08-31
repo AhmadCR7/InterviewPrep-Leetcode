d656d482e40d61c0bb96b3ea89c4ddfc51279345
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-08-31 08:42:10 -0700

    Time: 66 ms (68.47%), Space: 13.9 MB (76.14%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -6,9 +6,22 @@ class Solution:
         res = 0
         
         for i in range(len(s)):
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i +1]]:
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
                 res -= roman[s[i]]
             else:
                 res += roman[s[i]]
-        return res 
+        return res
         
+        
+'''
+Solution: 
+we use hashmap to map through the string 
+we go throgh the string for i in range(len(s))
+now we check are we substraction the if i + 1 < len(s) we don't want to be out of bound 
+and check what is the value at index[i] we can check that from roman[s[i]] and check if that 
+value is smaller than the next char roman[s[i+1]]
+if its less than that we subtract it res -= roman[s[i]]
+if not add else: res += roman[s[i]]
+return the result 
+
+'''
