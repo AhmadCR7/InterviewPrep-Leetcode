e3b9b8fcf5d9aef0f0bd5b5303b86a9706d09b0d
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-09-04 14:05:40 -0700

    Time: 62 ms (96.47%), Space: 15.2 MB (69.04%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,23 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        
         profit = 0
         
         for i in range(1, len(prices)):
             if prices[i] > prices[i-1]:
                 profit += (prices[i] - prices[i-1])
-                
         return profit 
         
-        
-        
-        
-'''
-
-since we can predect the fucture we can see when to buy or sell
-profit = 0
-pointer i we ignore the first value for i in range(1, len(prices))
-if prices[i] > prices[i-1]
-calculate the profite profit += (prices[i] - prices[i-1])
-return profit 
-
-'''
