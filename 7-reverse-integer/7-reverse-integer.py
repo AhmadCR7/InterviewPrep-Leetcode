532c2238cb0988d70c34bd4c968190320164908f
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-09-05 15:30:07 -0700

    Time: 65 ms (18.21%), Space: 14 MB (16.28%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -16,4 +16,27 @@ class Solution:
                 return 0
             res = (res*10) + digit 
         return res 
+    
+    '''
+    we set our res = 0
+    we chech for x while x:
+we take x and mod it by 10 becuase of python since -1 % 10 = 0 
+we use digit=int(math.fmod(x, 10))
+now we take the x and divid by 10 x = int(x/10) we use int becuase python -1//10 = -1
+
+we want the max value and devide by 10 except the last digit 
+if (res > MAX // 10 or (res == MAX // 10 and digit >= MAX % 10)): it will over flow so we return 0 
+if (res < MIN // 10 or (res == MIN // 10 and digit <= MIN % 10)): if the res is too small then return 0
+
+now take the res  = (res*10) + digit 
+return res 
+
+Time Complexity: O(log(num))
+
+Auxiliary Space: O(1)
+
+
+
+    
+    '''
         
