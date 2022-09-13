f2e58fd99db6006320ca91a6068bbbc1471b4054
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-09-13 11:26:28 -0700

    Time: 79 ms (8.64%), Space: 13.9 MB (89.89%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        counter = {"b": 0, "a": 0, "l": 0, "o": 0, "n":0}
+        
+        for char in text:
+            if char in counter:
+                counter[char] += 1
+        counter["l"] //= 2
+        counter["o"] //= 2
+        return min(counter.values())
+            
+            
+
+        
+        
+        
+'''
+A Counter is a container that keeps track of how many times equivalent values are added.
+counter = {"b":0, "a":0, "l":0, "o":0, "n":0}
+        for char in text:
+            if char in counter:
+                counter[char] += 1
+        counter["l"] //= 2
+        counter["o"] //= 2
+        return min(counter.values())
+'''
