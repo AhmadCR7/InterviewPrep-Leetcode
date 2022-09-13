6ef0ac8899368f0f5a977a6ca9391cb27ce6703a
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-09-13 11:32:24 -0700

    Time: 73 ms (14.67%), Space: 13.9 MB (89.89%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        counter = {"b": 0, "a": 0, "l": 0, "o": 0, "n":0}
-        
-        for char in text:
-            if char in counter:
-                counter[char] += 1
-        counter["l"] //= 2
-        counter["o"] //= 2
-        return min(counter.values())
+        word = {'a':0, 'b':0,'l':0, 'n':0, 'o':0}
+        double_char = ['l','o']
+        for c in text:
+            if c in word:
+                word[c] += 1
+        for c in double_char:
+            word[c] //= 2
+        return min(word.values())
             
             
 
@@ -23,4 +23,7 @@ counter = {"b":0, "a":0, "l":0, "o":0, "n":0}
         counter["l"] //= 2
         counter["o"] //= 2
         return min(counter.values())
+Intuitive Python Solution with O(n) time and O(1) space.
+
+
 '''
