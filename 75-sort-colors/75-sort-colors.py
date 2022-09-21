a4a6236e2b2332245bef95c8625c76e874f67544
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-09-21 13:58:05 -0700

    Time: 74 ms (11.00%), Space: 13.9 MB (15.85%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -16,5 +16,26 @@ class Solution:
                 p2 -= 1
             else:
                 curr += 1
+'''
+Algorithm
+Initialise the rightmost boundary of zeros : p0 = 0. During the algorithm execution nums[idx < p0] = 0.
+
+Initialise the leftmost boundary of twos : p2 = n - 1. During the algorithm execution nums[idx > p2] = 2.
+
+Initialise the index of current element to consider : curr = 0.
+
+While curr <= p2 :
+
+If nums[curr] = 0 : swap currth and p0th elements and move both pointers to the right.
+
+If nums[curr] = 2 : swap currth and p2th elements. Move pointer p2 to the left.
+
+If nums[curr] = 1 : move pointer curr to the right.
+
+Complexity Analysis
+
+Time complexity : \mathcal{O}(N)O(N) since it's one pass along the array of length NN.
+Space complexity : \mathcal{O}(1)O(1) since it's a constant space solution.
+'''
                 
         
