ef5aa62688471758e572011b6c8709cb3b5ffbf2
Ahmad Afghan <54687398+AhmadCR7@users.noreply.github.com>
2022-10-05 10:26:01 -0700

    Time: 116 ms (22.01%), Space: 14.5 MB (67.08%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,6 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        #return sorted(s) == sorted(t)
+        dic1, dic2 = [0] * 26, [0] * 26
+
+        for item in s:
+            dic1[ord(item) - ord('a')] += 1
+        for item in t:
+            dic2[ord(item) - ord('a')] += 1
+        return dic1 == dic2
         
         
 # you can also use built in function of return Count(s) == Count(t)
